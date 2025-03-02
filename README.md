c9da313e09f3ace3716bd31d407b2ad4640331f2
Seppo Enarvi <seppo.git@marjaniemi.com>
2025-03-02 10:26:05 +0200

    Added README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hydrolink integration for Home Assistant
+
+Hydrolink is a remote water meter reading service provided by Koka Oy. This integration reads the meter readings from the Hydrolink API.
---
+++ custom_components/hydrolink/const.py
@@ -0,0 +1,9 @@
+from random import randint
+
+DOMAIN = "hydrolink"
+RANDOM_MINUTE = randint(10, 30)
+RANDOM_SECOND = randint(0, 59)
+
+NAME = "Hydrolink"
+VERSION = "0.0.1"
+ISSUES_URL = "https://github.com/senarvi/hydrolink-home-assistant/issues"
